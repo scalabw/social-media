861aad5641cf7a601870c8be037413aaf55a7314
scalabw <william.scalabre@epitech.eu>
2019-04-11 02:43:39 +0700

    add Title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,23 +2,35 @@
 /* eslint-disable block-spacing */
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { blue, green } from '@material-ui/core/colors';
 import logo from '../../logo.svg';
 import './App.css';
 import SocialCard from '../SocialCard/SocialCard';
 // import HeaderBar from '../HeaderBar/HeaderBar';
 import datas from '../../data/data.json';
 
-const styles = {};
+const styles = {
+  title: {
+    color: green,
+    margin: '100px',
+  },
+};
+
+const titleStyle = {
+  color: green,
+};
 
 // TODO : create 2 other column (left & right) search for example online
 // TODO : create real header
 // TODO : rename project Social-List
+// TODO : change color title
 
-const App = () => {
+const App = classes => {
   return (
     <div className="App">
       {/* <HeaderBar /> */}
       <header className="App-header">
+        <p style={classes.title}>Social-Media</p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <div className="container">
